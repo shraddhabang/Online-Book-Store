efe8c3f328d253071424d2178ef22633050aa917
Shraddha <bangshraddha@gmail.com>
2019-04-12 17:18:46 -0500

    Updated UI template for Login

--- functions/database_functions.php
+++ functions/database_functions.php
@@ -1,6 +1,6 @@
 <?php
 	function db_connect(){
-		$conn = mysqli_connect("localhost", "root", "root", "www_project");
+		$conn = mysqli_connect("localhost", "root", "root", "bookstore");
 		if(!$conn){
 			echo "Can't connect database " . mysqli_connect_error($conn);
 			exit;
