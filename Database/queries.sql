2d90cce99ec050e4090b080eab1d7e55f9c18a10
Shraddha <bangshraddha@gmail.com>
2019-04-17 17:41:29 -0500

    Insert User

--- Database/queries.sql
+++ Database/queries.sql
@@ -8,3 +8,5 @@ CREATE TABLE IF NOT EXISTS `user` (
   `zip` varchar(10),
   `role` varchar(20) NOT NULL
 );
+
+INSERT INTO `user` (`user_id_pk`, `name`, `password`, `email`, `phone`, `city`, `zip`, `role`) VALUES (NULL, 'Shraddha', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'shb@gmail.com', '4692234198', 'Dallas', '75252', 'admin');
